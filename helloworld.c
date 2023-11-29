cbd3250115ceca3a785227af40bb1bb3c1a444bf
guilhermesilfer <guilhermeferreiradev14@gmail.com>
2023-11-29 14:37:56 -0300

    Fixed main function

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
 int main() {
-	pritnf("Hello, World!");
-=======
-void main() {
 	pritnf("Hello world!");
->>>>>>> 18bf5aac2862d77fdd17a3c5988dae8428bec3ec
 	return 0;
 }
