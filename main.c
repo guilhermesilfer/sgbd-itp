2f416fb6c4e0150642e2e3dc6796e6ca7ec0c8a7
João Lucas de Melo <joaolucasmm2512@gmail.com>
2023-11-30 18:34:04 -0300

    Exemplo do hello main
    só mudamos o nome do arquivo para não dar conflito com o arquivo main do projeto

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
-#include "hello.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-	say_hello();
-	say_hello();
-	say_hello();
-	return 0;
+int main(){
+    int op;
+    printf("\nSelecione um numero de 1 a 7 para prosseguir:\n\n");
+    printf("1 - Criar uma tabela\n");
+    printf("2 - Listar todas as tabelas\n");
+    printf("3 - Criar uma nova linha na tabela\n");
+    printf("4 - Listar todos os dados de uma tabela\n");
+    printf("5 - Pesquisar valor em uma tabela\n");
+    printf("6 - Apagar uma linha de uma taebla\n");
+    printf("7 - Apagar uma tabela\n");
+
+    scanf("%d", &op);
+
+    switch (op){
+    case 1:
+        break;
+
+    case 2:
+        break;
+
+    case 3:
+        break;
+
+    case 4:
+        break;
+
+    case 5:
+        break;
+
+    case 6:
+        break;
+        
+    case 7:
+        break;
+    
+    default: printf("Por favor selecione uma das opcoes validas!");
+        exit(1);
+        break;
+    }
+
+    return 0;
 }
