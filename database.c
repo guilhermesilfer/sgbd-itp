e3867eb4994e8513192df265a631216ea8aaf62b
guilhermesilfer <guilhermeferreiradev14@gmail.com>
2023-12-06 12:51:53 -0300

    criar_tabela() finalizada

--- database.c
+++ database.c
@@ -5,36 +5,31 @@
 //Caso 1 do menu
 void criar_tabela(){
     char nome[31];
-    printf("Digite o nome da tabela: \n");
-    scanf("%s", nome);
-    //fgets(nome, 31, stdin);
-    nome[strcspn(nome, "\n")] = '\0';
-    strcat(nome, ".txt");
-    printf("%c", nome[4]);
-    printf("%s", nome);
+    nome[0] = '\0';
 
+    printf("Digite o nome da tabela: \n");
+
+    scanf(" %[^\n]", nome);
+
+    strcat(nome, ".txt");
+
+    printf("%s\n", nome);
 
     FILE* tabela;
-    tabela = fopen("exemplo.txt", "w");
+    tabela = fopen(nome, "w");
+
     if (tabela == NULL) {
-        printf("Error ao abrir o arquivo: %s\n", strerror(errno));
+        printf("Erro ao abrir o arquivo: %s\n", strerror(errno));
         return;
     }
 
     fprintf(tabela, "testando");
-    int fcloseResult = 1;
-    fcloseResult = fclose(tabela);
 
-    printf("%d\n", fcloseResult);
-
-    if(fcloseResult != 0) {
+    if (fclose(tabela) != 0) {
         printf("Erro ao fechar o arquivo: %s\n", strerror(errno));
+    } else {
+        printf("Sucesso ao fechar o arquivo\n");
     }
-    else {
-        printf("Sucesso ao fechar o arquivo");
-    }
-
-    return;
 }
 
 //Caso 2 do Menu
