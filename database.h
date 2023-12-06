fbfd19a8e6926370c44b2d959563d659d750be8f
João Lucas de Melo <joaolucasmm2512@gmail.com>
2023-12-06 12:04:23 -0300

    Funções do programa
    Adicionei as funções que faltavam definir e a explicação do que cada uma faz

--- database.h
+++ database.h
@@ -1,7 +1,18 @@
 #ifndef DEF
 #define DEF
 
-void criar_tabela();
-void listar_tabelas();
+void criar_tabela(); //Cria uma tabela no programa
+
+void listar_tabelas(); //Lista todas as tabelas
+
+void criar_linha_tabela(); //Cria um registro /ou linha na tabela
+
+void listar_dados_tabela(); //Lista todos os dados de uma tabela (basicamente imprime tudo nela)
+
+void pesquisar_valor(); //Pesquisa um determinado valor dentro de uma tabela
+
+void apagar_linha(); //Apaga uma linha /ou registro de uma tabela
+
+void apagar_tabela(); //Apaga uma tabela
 
 #endif
